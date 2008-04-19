7bd01af3bed1572de8d092317b941480a2bca42b
ashmore <ashmore@a3f7fd50-973e-0410-8b28-ed0ccb69969c>
2008-04-19 04:59:58 +0000

    Updates to painter and included a nifty applet.

--- trunk/Painter/src/painter/functions/commands/BrushCommands.java
+++ trunk/Painter/src/painter/functions/commands/BrushCommands.java
@@ -53,6 +53,45 @@ public final class BrushCommands implements AllComponents<Command>, Described {
 
    }
 
+   public static class Plot_d2_r_col extends Command {
+
+      public int getNumberInputs() {
+         return 4;
+      }
+
+      public String getInputName(int i) {
+         switch(i) {
+            case 0: return "x";
+            case 1: return "y";
+            case 2: return "r";
+            case 3: return "color";
+            default: return null;
+         }
+
+      }
+
+      public Class getInputType(int i) {
+         switch(i) {
+            case 0: return LDouble.class;
+            case 1: return LDouble.class;
+            case 2: return LDouble.class;
+            case 3: return Color.class;
+            default: return null;
+         }
+
+      }
+
+      public void execute(Context context, Object[] inputs) {
+         LDouble x = (LDouble)inputs[0];
+         LDouble y = (LDouble)inputs[1];
+         LDouble r = (LDouble)inputs[2];
+         Color color = (Color)inputs[3];
+         Canvas canvas = ( Canvas ) context . getVariable ( "canvas" ) ;
+         new Brush ( ) . paint ( x . val , y . val , r . val , color , canvas ) ;
+      }
+
+   }
+
    public static class Plot_v2_col extends Command {
 
       public int getNumberInputs() {
@@ -86,6 +125,246 @@ public final class BrushCommands implements AllComponents<Command>, Described {
 
    }
 
+   public static class Plot_v2_r_col extends Command {
+
+      public int getNumberInputs() {
+         return 3;
+      }
+
+      public String getInputName(int i) {
+         switch(i) {
+            case 0: return "x";
+            case 1: return "r";
+            case 2: return "color";
+            default: return null;
+         }
+
+      }
+
+      public Class getInputType(int i) {
+         switch(i) {
+            case 0: return LVect2d.class;
+            case 1: return LDouble.class;
+            case 2: return Color.class;
+            default: return null;
+         }
+
+      }
+
+      public void execute(Context context, Object[] inputs) {
+         LVect2d x = (LVect2d)inputs[0];
+         LDouble r = (LDouble)inputs[1];
+         Color color = (Color)inputs[2];
+         Canvas canvas = ( Canvas ) context . getVariable ( "canvas" ) ;
+         new Brush ( ) . paint ( x . x , x . y , r . val , color , canvas ) ;
+      }
+
+   }
+
+   public static class Line_d4_col extends Command {
+
+      public int getNumberInputs() {
+         return 5;
+      }
+
+      public String getInputName(int i) {
+         switch(i) {
+            case 0: return "x1";
+            case 1: return "y1";
+            case 2: return "x2";
+            case 3: return "y2";
+            case 4: return "color";
+            default: return null;
+         }
+
+      }
+
+      public Class getInputType(int i) {
+         switch(i) {
+            case 0: return LDouble.class;
+            case 1: return LDouble.class;
+            case 2: return LDouble.class;
+            case 3: return LDouble.class;
+            case 4: return Color.class;
+            default: return null;
+         }
+
+      }
+
+      public void execute(Context context, Object[] inputs) {
+         LDouble x1 = (LDouble)inputs[0];
+         LDouble y1 = (LDouble)inputs[1];
+         LDouble x2 = (LDouble)inputs[2];
+         LDouble y2 = (LDouble)inputs[3];
+         Color color = (Color)inputs[4];
+         Canvas canvas = ( Canvas ) context . getVariable ( "canvas" ) ;
+         new Brush ( ) . paintLine ( x1 . val , y1 . val , x2 . val , y2 . val , .02 , .02 , color , canvas ) ;
+      }
+
+   }
+
+   public static class Line_d4_r_col extends Command {
+
+      public int getNumberInputs() {
+         return 6;
+      }
+
+      public String getInputName(int i) {
+         switch(i) {
+            case 0: return "x1";
+            case 1: return "y1";
+            case 2: return "x2";
+            case 3: return "y2";
+            case 4: return "r";
+            case 5: return "color";
+            default: return null;
+         }
+
+      }
+
+      public Class getInputType(int i) {
+         switch(i) {
+            case 0: return LDouble.class;
+            case 1: return LDouble.class;
+            case 2: return LDouble.class;
+            case 3: return LDouble.class;
+            case 4: return LDouble.class;
+            case 5: return Color.class;
+            default: return null;
+         }
+
+      }
+
+      public void execute(Context context, Object[] inputs) {
+         LDouble x1 = (LDouble)inputs[0];
+         LDouble y1 = (LDouble)inputs[1];
+         LDouble x2 = (LDouble)inputs[2];
+         LDouble y2 = (LDouble)inputs[3];
+         LDouble r = (LDouble)inputs[4];
+         Color color = (Color)inputs[5];
+         Canvas canvas = ( Canvas ) context . getVariable ( "canvas" ) ;
+         new Brush ( ) . paintLine ( x1 . val , y1 . val , x2 . val , y2 . val , r . val , r . val , color , canvas ) ;
+      }
+
+   }
+
+   public static class Line_v2_v2_col extends Command {
+
+      public int getNumberInputs() {
+         return 3;
+      }
+
+      public String getInputName(int i) {
+         switch(i) {
+            case 0: return "x1";
+            case 1: return "x2";
+            case 2: return "color";
+            default: return null;
+         }
+
+      }
+
+      public Class getInputType(int i) {
+         switch(i) {
+            case 0: return LVect2d.class;
+            case 1: return LVect2d.class;
+            case 2: return Color.class;
+            default: return null;
+         }
+
+      }
+
+      public void execute(Context context, Object[] inputs) {
+         LVect2d x1 = (LVect2d)inputs[0];
+         LVect2d x2 = (LVect2d)inputs[1];
+         Color color = (Color)inputs[2];
+         Canvas canvas = ( Canvas ) context . getVariable ( "canvas" ) ;
+         new Brush ( ) . paintLine ( x1 . x , x1 . y , x2 . x , x2 . y , .02 , .02 , color , canvas ) ;
+      }
+
+   }
+
+   public static class Line_v2_v2_r_col extends Command {
+
+      public int getNumberInputs() {
+         return 4;
+      }
+
+      public String getInputName(int i) {
+         switch(i) {
+            case 0: return "x1";
+            case 1: return "x2";
+            case 2: return "r";
+            case 3: return "color";
+            default: return null;
+         }
+
+      }
+
+      public Class getInputType(int i) {
+         switch(i) {
+            case 0: return LVect2d.class;
+            case 1: return LVect2d.class;
+            case 2: return LDouble.class;
+            case 3: return Color.class;
+            default: return null;
+         }
+
+      }
+
+      public void execute(Context context, Object[] inputs) {
+         LVect2d x1 = (LVect2d)inputs[0];
+         LVect2d x2 = (LVect2d)inputs[1];
+         LDouble r = (LDouble)inputs[2];
+         Color color = (Color)inputs[3];
+         Canvas canvas = ( Canvas ) context . getVariable ( "canvas" ) ;
+         new Brush ( ) . paintLine ( x1 . x , x1 . y , x2 . x , x2 . y , r . val , r . val , color , canvas ) ;
+      }
+
+   }
+
+   public static class Line_v2_v2_r2_col extends Command {
+
+      public int getNumberInputs() {
+         return 5;
+      }
+
+      public String getInputName(int i) {
+         switch(i) {
+            case 0: return "x1";
+            case 1: return "x2";
+            case 2: return "r1";
+            case 3: return "r2";
+            case 4: return "color";
+            default: return null;
+         }
+
+      }
+
+      public Class getInputType(int i) {
+         switch(i) {
+            case 0: return LVect2d.class;
+            case 1: return LVect2d.class;
+            case 2: return LDouble.class;
+            case 3: return LDouble.class;
+            case 4: return Color.class;
+            default: return null;
+         }
+
+      }
+
+      public void execute(Context context, Object[] inputs) {
+         LVect2d x1 = (LVect2d)inputs[0];
+         LVect2d x2 = (LVect2d)inputs[1];
+         LDouble r1 = (LDouble)inputs[2];
+         LDouble r2 = (LDouble)inputs[3];
+         Color color = (Color)inputs[4];
+         Canvas canvas = ( Canvas ) context . getVariable ( "canvas" ) ;
+         new Brush ( ) . paintLine ( x1 . x , x1 . y , x2 . x , x2 . y , r1 . val , r2 . val , color , canvas ) ;
+      }
+
+   }
+
    public String getDescription() {
       return "Brush commands";
    }
@@ -93,7 +372,14 @@ public final class BrushCommands implements AllComponents<Command>, Described {
    public List<Command> allInstances(ContextModel cm) {
       List<Command> r = new ArrayList<Command>();
       r.add(new Plot_d2_col());
+      r.add(new Plot_d2_r_col());
       r.add(new Plot_v2_col());
+      r.add(new Plot_v2_r_col());
+      r.add(new Line_d4_col());
+      r.add(new Line_d4_r_col());
+      r.add(new Line_v2_v2_col());
+      r.add(new Line_v2_v2_r_col());
+      r.add(new Line_v2_v2_r2_col());
       return r;
    }
 
--- trunk/Painter/src/painter/tools/brush/Brush.java
+++ trunk/Painter/src/painter/tools/brush/Brush.java
@@ -15,7 +15,7 @@ public class Brush {
 
     public void paint(double x, double y, double radius, Color color, Canvas canvas) {
 
-        System.out.println("paint: "+x+", "+y+" ("+radius+"): "+color);
+        //System.out.println("paint: "+x+", "+y+" ("+radius+"): "+color);
         
         x = x % 1.0;
         if (x < 0) {
@@ -27,6 +27,11 @@ public class Brush {
             y += 1.0;
         }
 
+        drawDab(x, y, radius, color, canvas);
+    }
+    
+    private void drawDab (double x, double y, double radius, Color color, Canvas canvas) {
+    
         int radiusSteps = 10;
         int thetaSteps = 30;
 
@@ -41,6 +46,33 @@ public class Brush {
                 canvas.setColor(x1, y1, color);
             }
         }
+    }
+    
+    public void paintLine(double x1, double y1, double x2, double y2, double radius1, double radius2, Color color, Canvas canvas) {
 
+        //System.out.println("line: "+x1+", "+y1+", "+x2+", "+y2+" ("+radius1+", "+radius2+"): "+color);
+        
+        x1 = x1 % 1.0;
+        if (x1 < 0) {
+            x1 += 1.0;
+        }
+
+        y1 = y1 % 1.0;
+        if (y1 < 0) {
+            y1 += 1.0;
+        }
+
+        double xd = x1-x2;
+        double yd = y1-y2;
+        
+        int lineSteps = (int) (20*Math.sqrt(xd*xd+yd*yd));
+        
+        for(int i=0; i< lineSteps; i++) {
+            double t = (double)i/lineSteps;
+            double x = t*x2+(1-t)*x1;
+            double y = t*y2+(1-t)*y1;
+            double radius = t*radius2+(1-t)*radius1;
+            drawDab(x, y, radius, color, canvas);
+        }
     }
 }

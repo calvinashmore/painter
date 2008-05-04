51668868cbafaea47b8f4ec0f4d22e2fc77a3952
ashmore <ashmore@a3f7fd50-973e-0410-8b28-ed0ccb69969c>
2008-05-04 05:23:27 +0000

    Added some implementations of mutators. These have not been tested yet.

---
+++ GeneticBase/src/genetic/component/statementlist/mutator/MutateStatement.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package genetic.component.statementlist.mutator;
+
+import genetic.BuildException;
+import genetic.Foundation;
+import genetic.GeneticFoundation;
+import genetic.MutatorAction;
+import genetic.component.statement.Statement;
+import genetic.component.statementlist.StatementList;
+import java.util.List;
+
+/**
+ *
+ * @author Calvin Ashmore
+ */
+public class MutateStatement extends MutatorAction<StatementList> {
+
+    @Override
+    public boolean mutate(StatementList target) throws BuildException {
+        List<Statement> statements = target.getStatements();
+        GeneticFoundation foundation = Foundation.getInstance();
+
+        int index = foundation.getBuilderRandom().nextInt(statements.size());
+        return foundation.getStatementMutator().mutate(statements.get(index));
+    }
+}

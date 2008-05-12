598e77c611136921d6fd2ed29d2f4d31c8ac3d9d
ashmore <ashmore@a3f7fd50-973e-0410-8b28-ed0ccb69969c>
2008-05-12 02:46:42 +0000

    Update should fix expression mutators so that they can replace from a root instead of sub-expressions.

--- GeneticBase/src/genetic/component/expression/ExpressionUtils.java
+++ GeneticBase/src/genetic/component/expression/ExpressionUtils.java
@@ -5,6 +5,7 @@
 package genetic.component.expression;
 
 import genetic.BuildException;
+import genetic.component.statement.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -41,7 +42,6 @@ public class ExpressionUtils {
     }
 
     /**
-     * root must NOT be same as replaced.
      * 
      * @param root
      * @param replaced
@@ -49,18 +49,35 @@ public class ExpressionUtils {
      * @param cm
      * @throws genetic.util.BuildException
      */
-    public static void replaceExpression(Expression expression, Expression replaced, Expression replacer) throws BuildException {
+    public static void replaceExpression(Expression root, Expression replaced, Expression replacer) throws BuildException {
 
-        for(Expression child : gatherNodes(expression)) {
-            
+        if (root == replaced) {
+            // we are replacing the root itself, so we need to change the parent of root.
+
+            // root has an expression parent, just use that
+            if (root.getParent() instanceof Expression) {
+                replaceExpression((Expression) root.getParent(), replaced, replacer);
+                return;
+            }
+
+            // otherwise, the parent should be a statement.
+            Statement parent = (Statement) root.getParent();
+            for (int i = 0; i < parent.getFunction().getNumberInputs(); i++) {
+                if (parent.getInput(i) == root) {
+                    parent.setInput(i, replacer);
+                }
+            }
+
+            return;
+        }
+
+        for (Expression child : gatherNodes(root)) {
             List<Expression> subChildren = child.getChildren();
-            for(int i=0; i<subChildren.size(); i++) {
-                
-                if(subChildren.get(i) == replaced) {
+            for (int i = 0; i < subChildren.size(); i++) {
+                if (subChildren.get(i) == replaced) {
                     child.setInput(i, replacer);
                 }
             }
         }
     }
-
 }

f620d3f2d70bdf1656cee779337daa4aaaf7eb16
ashmore <ashmore@a3f7fd50-973e-0410-8b28-ed0ccb69969c>
2009-11-13 02:45:28 +0000

    Modifies painter structure to use a separate tools library. Beginning to work on the tools.

---
+++ trunk/PainterTools/src/painter/tools/brush/Brush.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package painter.tools.brush;
+
+import painter.tools.canvas.Canvas;
+import utils.linear.Color;
+
+/**
+ *
+ * @author Calvin Ashmore
+ */
+public interface Brush {
+
+    void paint(double x, double y, double dx, double dy, double radius, Color color, Canvas canvas);
+
+}
--- trunk/PainterTools/src/painter/tools/canvas/Canvas.java
+++ trunk/PainterTools/src/painter/tools/canvas/Canvas.java
@@ -4,8 +4,8 @@
  */
 package painter.tools.canvas;
 
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
-import java.nio.DoubleBuffer;
 import utils.linear.Color;
 
 /**
@@ -14,25 +14,40 @@ import utils.linear.Color;
  */
 public class Canvas {
 
-    private DoubleBuffer dataBuffer;
+    //private DoubleBuffer dataBuffer;
+    private BufferedImage image;
     private int width;
     private int height;
-    private int channels;
+    //private int channels;
 
     public Canvas(int width, int height) {
         this.width = width;
         this.height = height;
-        this.channels = 3;
+        //this.channels = 3;
 
-        dataBuffer = DoubleBuffer.allocate(channels * width * height);
+        //dataBuffer = DoubleBuffer.allocate(channels * width * height);
+        image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
     }
 
     private Color getColor(int x, int y) {
-        double data[] = new double[3];
-        int offset = 3 * (x + width * y);
-        dataBuffer.position(offset);
-        dataBuffer.get(data, 0, 3);
-        return new Color(data[0], data[1], data[2]);
+//        double data[] = new double[3];
+//        int offset = 3 * (x + width * y);
+//        dataBuffer.position(offset);
+//        dataBuffer.get(data, 0, 3);
+        return Color.fromRGB(image.getRGB(x, y));
+        //return new Color(data[0], data[1], data[2]);
+    }
+
+    public Graphics2D getGraphics() {
+        return image.createGraphics();
     }
 
     public BufferedImage makeImage() {
@@ -62,13 +77,15 @@ public class Canvas {
             return;
         }
         
-        double data[] = new double[3];
-        int offset = 3 * (ix + width * iy);
-        data[0] = color.r;
-        data[1] = color.g;
-        data[2] = color.b;
+//        double data[] = new double[3];
+//        //int offset = 3 * (ix + width * iy);
+//        data[0] = color.r;
+//        data[1] = color.g;
+//        data[2] = color.b;
+
+        image.setRGB(ix, iy, color.toARGB());
         
-        dataBuffer.position(offset);
-        dataBuffer.put(data, 0, 3);
+//        dataBuffer.position(offset);
+//        dataBuffer.put(data, 0, 3);
     }
 }

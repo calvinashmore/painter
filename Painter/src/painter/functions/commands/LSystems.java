04aa61ea8cc6db329ea12ed6b60ca5fb6a332651
ashmore <ashmore@a3f7fd50-973e-0410-8b28-ed0ccb69969c>
2010-05-07 21:02:17 +0000

    Lots more L-system stuff.

--- Painter/src/painter/functions/commands/LSystems.java
+++ Painter/src/painter/functions/commands/LSystems.java
@@ -22,7 +22,7 @@ import painter.tools.stroke.*;
 
 public final class LSystems implements AllComponents<Command>, Described {
 
-   public static class RenderLSystem extends Command {
+   public static class RenderLSystem1 extends Command {
 
       private LDouble width;private LDouble initialTheta;private LDouble scale;private LVect2d initialPosition;
       public int getNumberParameters() {
@@ -101,7 +101,7 @@ public final class LSystems implements AllComponents<Command>, Described {
 
       }
 
-      public RenderLSystem() {
+      public RenderLSystem1() {
          addGroupMeta(this);
          addMeta("selectionWeight", 100);
          width = new LDouble ( Math . random ( ) * .1 ) ;
@@ -130,13 +130,458 @@ public final class LSystems implements AllComponents<Command>, Described {
 
    }
 
+   public static class RenderLSystem2 extends Command {
+
+      private LDouble width;private LDouble initialTheta;private LDouble scale;private LVect2d initialPosition;
+      public int getNumberParameters() {
+         return 4;
+      }
+
+      public Object getParameter(int i) {
+         switch(i) {
+            case 0: return width;
+            case 1: return initialTheta;
+            case 2: return scale;
+            case 3: return initialPosition;
+            default: return null;
+         }
+
+      }
+
+      public String getParameterName(int i) {
+         switch(i) {
+            case 0: return "width";
+            case 1: return "initialTheta";
+            case 2: return "scale";
+            case 3: return "initialPosition";
+            default: return null;
+         }
+
+      }
+
+      public Class getParameterType(int i) {
+         switch(i) {
+            case 0: return LDouble.class;
+            case 1: return LDouble.class;
+            case 2: return LDouble.class;
+            case 3: return LVect2d.class;
+            default: return null;
+         }
+
+      }
+
+      public void setParameter(int i, Object value) {
+         switch(i) {
+            case 0: width = (LDouble) value; return;
+            case 1: initialTheta = (LDouble) value; return;
+            case 2: scale = (LDouble) value; return;
+            case 3: initialPosition = (LVect2d) value; return;
+            default: return;
+         }
+
+      }
+
+      public int getNumberInputs() {
+         return 5;
+      }
+
+      public String getInputName(int i) {
+         switch(i) {
+            case 0: return "string";
+            case 1: return "color";
+            case 2: return "brush";
+            case 3: return "stroke";
+            case 4: return "renderer";
+            default: return null;
+         }
+
+      }
+
+      public Class getInputType(int i) {
+         switch(i) {
+            case 0: return LSystemString.class;
+            case 1: return CurveUtil . Curve_col.class;
+            case 2: return Brush.class;
+            case 3: return Stroke.class;
+            case 4: return SegmentRenderer.class;
+            default: return null;
+         }
+
+      }
+
+      public RenderLSystem2() {
+         addGroupMeta(this);
+         addMeta("selectionWeight", 5);
+         width = new LDouble ( Math . random ( ) * .1 ) ;
+         initialTheta = new LDouble ( Math . random ( ) * Math . PI * 2 ) ;
+         initialPosition = new LVect2d ( Math . random ( ) , Math . random ( ) ) ;
+         scale = new LDouble ( 1 + Math . random ( ) ) ;
+      }
+
+      public void execute(Context context, Object[] inputs) {
+         final LSystemString string = (LSystemString)inputs[0];
+         final CurveUtil . Curve_col color = (CurveUtil . Curve_col)inputs[1];
+         final Brush brush = (Brush)inputs[2];
+         final Stroke stroke = (Stroke)inputs[3];
+         final SegmentRenderer renderer = (SegmentRenderer)inputs[4];
+         Canvas canvas = ( Canvas ) context . getVariable ( "canvas" ) ;
+         LSystemRenderer lRenderer = new LSystemRenderer ( ) ;
+         lRenderer . setRotateSize ( string . getAngle ( ) ) ;
+         lRenderer . setInitialCoordinates ( initialPosition . x , initialPosition . y , initialTheta . val ) ;
+         lRenderer . setScale ( scale . val ) ;
+         List < CurveUtil . Curve_v2 > curves = lRenderer . process ( string . getString ( ) ) ;
+         for ( CurveUtil . Curve_v2 position : curves ) {
+         stroke . setSteps ( 100 ) ;
+         stroke . render ( brush , new ConstantCurve < LDouble > ( width ) , position , color , canvas , renderer ) ;
+         }
+      }
+
+   }
+
+   public static class RenderLSystem3 extends Command {
+
+      private LDouble initialTheta;private LDouble scale;private LVect2d initialPosition;
+      public int getNumberParameters() {
+         return 3;
+      }
+
+      public Object getParameter(int i) {
+         switch(i) {
+            case 0: return initialTheta;
+            case 1: return scale;
+            case 2: return initialPosition;
+            default: return null;
+         }
+
+      }
+
+      public String getParameterName(int i) {
+         switch(i) {
+            case 0: return "initialTheta";
+            case 1: return "scale";
+            case 2: return "initialPosition";
+            default: return null;
+         }
+
+      }
+
+      public Class getParameterType(int i) {
+         switch(i) {
+            case 0: return LDouble.class;
+            case 1: return LDouble.class;
+            case 2: return LVect2d.class;
+            default: return null;
+         }
+
+      }
+
+      public void setParameter(int i, Object value) {
+         switch(i) {
+            case 0: initialTheta = (LDouble) value; return;
+            case 1: scale = (LDouble) value; return;
+            case 2: initialPosition = (LVect2d) value; return;
+            default: return;
+         }
+
+      }
+
+      public int getNumberInputs() {
+         return 6;
+      }
+
+      public String getInputName(int i) {
+         switch(i) {
+            case 0: return "string";
+            case 1: return "color";
+            case 2: return "brush";
+            case 3: return "stroke";
+            case 4: return "renderer";
+            case 5: return "width";
+            default: return null;
+         }
+
+      }
+
+      public Class getInputType(int i) {
+         switch(i) {
+            case 0: return LSystemString.class;
+            case 1: return CurveUtil . Curve_col.class;
+            case 2: return Brush.class;
+            case 3: return Stroke.class;
+            case 4: return SegmentRenderer.class;
+            case 5: return CurveUtil . Curve_d.class;
+            default: return null;
+         }
+
+      }
+
+      public RenderLSystem3() {
+         addGroupMeta(this);
+         addMeta("selectionWeight", 5);
+         initialTheta = new LDouble ( Math . random ( ) * Math . PI * 2 ) ;
+         initialPosition = new LVect2d ( Math . random ( ) , Math . random ( ) ) ;
+         scale = new LDouble ( 1 + Math . random ( ) ) ;
+      }
+
+      public void execute(Context context, Object[] inputs) {
+         final LSystemString string = (LSystemString)inputs[0];
+         final CurveUtil . Curve_col color = (CurveUtil . Curve_col)inputs[1];
+         final Brush brush = (Brush)inputs[2];
+         final Stroke stroke = (Stroke)inputs[3];
+         final SegmentRenderer renderer = (SegmentRenderer)inputs[4];
+         final CurveUtil . Curve_d width = (CurveUtil . Curve_d)inputs[5];
+         Canvas canvas = ( Canvas ) context . getVariable ( "canvas" ) ;
+         LSystemRenderer lRenderer = new LSystemRenderer ( ) ;
+         lRenderer . setRotateSize ( string . getAngle ( ) ) ;
+         lRenderer . setInitialCoordinates ( initialPosition . x , initialPosition . y , initialTheta . val ) ;
+         lRenderer . setScale ( scale . val ) ;
+         List < CurveUtil . Curve_v2 > curves = lRenderer . process ( string . getString ( ) ) ;
+         for ( CurveUtil . Curve_v2 position : curves ) {
+         stroke . setSteps ( 100 ) ;
+         stroke . render ( brush , width , position , color , canvas , renderer ) ;
+         }
+      }
+
+   }
+
+   public static class RenderLSystem4 extends Command {
+
+      private LDouble width;private LDouble initialTheta;private LDouble scale;private LVect2d initialPosition;
+      public int getNumberParameters() {
+         return 4;
+      }
+
+      public Object getParameter(int i) {
+         switch(i) {
+            case 0: return width;
+            case 1: return initialTheta;
+            case 2: return scale;
+            case 3: return initialPosition;
+            default: return null;
+         }
+
+      }
+
+      public String getParameterName(int i) {
+         switch(i) {
+            case 0: return "width";
+            case 1: return "initialTheta";
+            case 2: return "scale";
+            case 3: return "initialPosition";
+            default: return null;
+         }
+
+      }
+
+      public Class getParameterType(int i) {
+         switch(i) {
+            case 0: return LDouble.class;
+            case 1: return LDouble.class;
+            case 2: return LDouble.class;
+            case 3: return LVect2d.class;
+            default: return null;
+         }
+
+      }
+
+      public void setParameter(int i, Object value) {
+         switch(i) {
+            case 0: width = (LDouble) value; return;
+            case 1: initialTheta = (LDouble) value; return;
+            case 2: scale = (LDouble) value; return;
+            case 3: initialPosition = (LVect2d) value; return;
+            default: return;
+         }
+
+      }
+
+      public int getNumberInputs() {
+         return 5;
+      }
+
+      public String getInputName(int i) {
+         switch(i) {
+            case 0: return "string";
+            case 1: return "color";
+            case 2: return "brush";
+            case 3: return "stroke";
+            case 4: return "renderer";
+            default: return null;
+         }
+
+      }
+
+      public Class getInputType(int i) {
+         switch(i) {
+            case 0: return LSystemString.class;
+            case 1: return CurveUtil . Curve_col.class;
+            case 2: return Brush.class;
+            case 3: return Stroke.class;
+            case 4: return SegmentRenderer.class;
+            default: return null;
+         }
+
+      }
+
+      public RenderLSystem4() {
+         addGroupMeta(this);
+         addMeta("selectionWeight", 5);
+         width = new LDouble ( Math . random ( ) * .1 ) ;
+         initialTheta = new LDouble ( Math . random ( ) * Math . PI * 2 ) ;
+         initialPosition = new LVect2d ( Math . random ( ) , Math . random ( ) ) ;
+         scale = new LDouble ( 1 + Math . random ( ) ) ;
+      }
+
+      public void execute(Context context, Object[] inputs) {
+         final LSystemString string = (LSystemString)inputs[0];
+         final CurveUtil . Curve_col color = (CurveUtil . Curve_col)inputs[1];
+         final Brush brush = (Brush)inputs[2];
+         final Stroke stroke = (Stroke)inputs[3];
+         final SegmentRenderer renderer = (SegmentRenderer)inputs[4];
+         Canvas canvas = ( Canvas ) context . getVariable ( "canvas" ) ;
+         LSystemRenderer lRenderer = new LSystemRenderer ( ) ;
+         lRenderer . setRotateSize ( string . getAngle ( ) ) ;
+         lRenderer . setInitialCoordinates ( initialPosition . x , initialPosition . y , initialTheta . val ) ;
+         lRenderer . setScale ( scale . val ) ;
+         List < CurveUtil . Curve_v2 > curves = lRenderer . process ( string . getString ( ) ) ;
+         double span = 1.0 / curves . size ( ) ;
+         for ( int i = 0 ;
+         i < curves . size ( ) ;
+         i ++ ) {
+         CurveUtil . Curve_v2 position = curves . get ( i ) ;
+         double t = i * span ;
+         Curve < Color > colorSegment = CurveUtil . subCurve ( color , t , span ) ;
+         stroke . setSteps ( 100 ) ;
+         stroke . render ( brush , new ConstantCurve < LDouble > ( width ) , position , colorSegment , canvas , renderer ) ;
+         }
+      }
+
+   }
+
+   public static class RenderLSystem5 extends Command {
+
+      private LDouble initialTheta;private LDouble scale;private LVect2d initialPosition;
+      public int getNumberParameters() {
+         return 3;
+      }
+
+      public Object getParameter(int i) {
+         switch(i) {
+            case 0: return initialTheta;
+            case 1: return scale;
+            case 2: return initialPosition;
+            default: return null;
+         }
+
+      }
+
+      public String getParameterName(int i) {
+         switch(i) {
+            case 0: return "initialTheta";
+            case 1: return "scale";
+            case 2: return "initialPosition";
+            default: return null;
+         }
+
+      }
+
+      public Class getParameterType(int i) {
+         switch(i) {
+            case 0: return LDouble.class;
+            case 1: return LDouble.class;
+            case 2: return LVect2d.class;
+            default: return null;
+         }
+
+      }
+
+      public void setParameter(int i, Object value) {
+         switch(i) {
+            case 0: initialTheta = (LDouble) value; return;
+            case 1: scale = (LDouble) value; return;
+            case 2: initialPosition = (LVect2d) value; return;
+            default: return;
+         }
+
+      }
+
+      public int getNumberInputs() {
+         return 6;
+      }
+
+      public String getInputName(int i) {
+         switch(i) {
+            case 0: return "string";
+            case 1: return "color";
+            case 2: return "brush";
+            case 3: return "stroke";
+            case 4: return "renderer";
+            case 5: return "width";
+            default: return null;
+         }
+
+      }
+
+      public Class getInputType(int i) {
+         switch(i) {
+            case 0: return LSystemString.class;
+            case 1: return CurveUtil . Curve_col.class;
+            case 2: return Brush.class;
+            case 3: return Stroke.class;
+            case 4: return SegmentRenderer.class;
+            case 5: return CurveUtil . Curve_d.class;
+            default: return null;
+         }
+
+      }
+
+      public RenderLSystem5() {
+         addGroupMeta(this);
+         addMeta("selectionWeight", 5);
+         initialTheta = new LDouble ( Math . random ( ) * Math . PI * 2 ) ;
+         initialPosition = new LVect2d ( Math . random ( ) , Math . random ( ) ) ;
+         scale = new LDouble ( 1 + Math . random ( ) ) ;
+      }
+
+      public void execute(Context context, Object[] inputs) {
+         final LSystemString string = (LSystemString)inputs[0];
+         final CurveUtil . Curve_col color = (CurveUtil . Curve_col)inputs[1];
+         final Brush brush = (Brush)inputs[2];
+         final Stroke stroke = (Stroke)inputs[3];
+         final SegmentRenderer renderer = (SegmentRenderer)inputs[4];
+         final CurveUtil . Curve_d width = (CurveUtil . Curve_d)inputs[5];
+         Canvas canvas = ( Canvas ) context . getVariable ( "canvas" ) ;
+         LSystemRenderer lRenderer = new LSystemRenderer ( ) ;
+         lRenderer . setRotateSize ( string . getAngle ( ) ) ;
+         lRenderer . setInitialCoordinates ( initialPosition . x , initialPosition . y , initialTheta . val ) ;
+         lRenderer . setScale ( scale . val ) ;
+         List < CurveUtil . Curve_v2 > curves = lRenderer . process ( string . getString ( ) ) ;
+         double span = 1.0 / curves . size ( ) ;
+         for ( int i = 0 ;
+         i < curves . size ( ) ;
+         i ++ ) {
+         CurveUtil . Curve_v2 position = curves . get ( i ) ;
+         double t = i * span ;
+         Curve < Color > colorSegment = CurveUtil . subCurve ( color , t , span ) ;
+         Curve < LDouble > widthSegment = CurveUtil . subCurve ( width , t , span ) ;
+         stroke . setSteps ( 100 ) ;
+         stroke . render ( brush , widthSegment , position , colorSegment , canvas , renderer ) ;
+         }
+      }
+
+   }
+
    public String getDescription() {
       return "LSystem commands";
    }
 
    public List<Command> allInstances(ContextModel cm) {
       List<Command> r = new ArrayList<Command>();
-      r.add(new RenderLSystem());
+      r.add(new RenderLSystem1());
+      r.add(new RenderLSystem2());
+      r.add(new RenderLSystem3());
+      r.add(new RenderLSystem4());
+      r.add(new RenderLSystem5());
       return r;
    }
 
--- Painter/src/painter/functions/expressions/LSystems.java
+++ Painter/src/painter/functions/expressions/LSystems.java
@@ -19,6 +19,161 @@ import painter.tools.lsystem.*;
 
 public final class LSystems implements AllComponents<ExpressionFunction>, Described {
 
+   public static class PermuteAngles extends ExpressionFunction {
+
+      private LDouble angleMultiplier;
+      public int getNumberParameters() {
+         return 1;
+      }
+
+      public Object getParameter(int i) {
+         switch(i) {
+            case 0: return angleMultiplier;
+            default: return null;
+         }
+
+      }
+
+      public String getParameterName(int i) {
+         switch(i) {
+            case 0: return "angleMultiplier";
+            default: return null;
+         }
+
+      }
+
+      public Class getParameterType(int i) {
+         switch(i) {
+            case 0: return LDouble.class;
+            default: return null;
+         }
+
+      }
+
+      public void setParameter(int i, Object value) {
+         switch(i) {
+            case 0: angleMultiplier = (LDouble) value; return;
+            default: return;
+         }
+
+      }
+
+      public int getNumberInputs() {
+         return 1;
+      }
+
+      public String getInputName(int i) {
+         switch(i) {
+            case 0: return "ls";
+            default: return null;
+         }
+
+      }
+
+      public Class getInputType(int i) {
+         switch(i) {
+            case 0: return LSystemString.class;
+            default: return null;
+         }
+
+      }
+
+      public PermuteAngles() {
+         addGroupMeta(this);
+         angleMultiplier = new LDouble ( .6 + .8 * Math . random ( ) ) ;
+      }
+
+      public LSystemString evaluate(Context context, Object[] inputs) {
+         final LSystemString ls = (LSystemString)inputs[0];
+         return new LSystemString ( ls . getString ( ) , angleMultiplier . val * ls . getAngle ( ) ) ;
+      }
+
+      public Class getReturnType() {
+         return LSystemString.class;
+      }
+
+   }
+
+   public static class LevyC extends ExpressionFunction {
+
+      public LevyC() {
+         addGroupMeta(this);
+      }
+
+      public LSystemString evaluate(Context context, Object[] inputs) {
+
+         LSystem lSystem = new LSystem ( ) ;
+         lSystem . addRule ( 'F' , "+F--F+" ) ;
+         String out = lSystem . apply ( "F" , 8 ) ;
+         return new LSystemString ( out , Math . PI / 4 ) ;
+      }
+
+      public Class getReturnType() {
+         return LSystemString.class;
+      }
+
+   }
+
+   public static class Pentigree extends ExpressionFunction {
+
+      public Pentigree() {
+         addGroupMeta(this);
+      }
+
+      public LSystemString evaluate(Context context, Object[] inputs) {
+
+         LSystem lSystem = new LSystem ( ) ;
+         lSystem . addRule ( 'F' , "F-F++F+F-F-F" ) ;
+         String out = lSystem . apply ( "F-F-F-F-F" , 4 ) ;
+         return new LSystemString ( out , 2 * Math . PI / 5 ) ;
+      }
+
+      public Class getReturnType() {
+         return LSystemString.class;
+      }
+
+   }
+
+   public static class IslandLake extends ExpressionFunction {
+
+      public IslandLake() {
+         addGroupMeta(this);
+      }
+
+      public LSystemString evaluate(Context context, Object[] inputs) {
+
+         LSystem lSystem = new LSystem ( ) ;
+         lSystem . addRule ( 'F' , "F-f+FF-F-FF-Ff-FF+f-FF+F+FF+Ff+FFF" ) ;
+         String out = lSystem . apply ( "F" , 2 ) ;
+         return new LSystemString ( out , Math . PI / 2 ) ;
+      }
+
+      public Class getReturnType() {
+         return LSystemString.class;
+      }
+
+   }
+
+   public static class KochIsland1 extends ExpressionFunction {
+
+      public KochIsland1() {
+         addGroupMeta(this);
+      }
+
+      public LSystemString evaluate(Context context, Object[] inputs) {
+
+         LSystem lSystem = new LSystem ( ) ;
+         lSystem . addRule ( 'F' , "F+F-F-FF+F+F-F" ) ;
+         String out = lSystem . apply ( "F" , 3 ) ;
+         return new LSystemString ( out , Math . PI / 2 ) ;
+      }
+
+      public Class getReturnType() {
+         return LSystemString.class;
+      }
+
+   }
+
    public static class Koch1 extends ExpressionFunction {
 
       public Koch1() {
@@ -50,6 +205,107 @@ public final class LSystems implements AllComponents<ExpressionFunction>, Descri
          LSystem lSystem = new LSystem ( ) ;
          lSystem . addRule ( 'F' , "F+F--F+F" ) ;
          String out = lSystem . apply ( "F" , 5 ) ;
+         return new LSystemString ( out , Math . PI / 6 ) ;
+      }
+
+      public Class getReturnType() {
+         return LSystemString.class;
+      }
+
+   }
+
+   public static class Koch3 extends ExpressionFunction {
+
+      public Koch3() {
+         addGroupMeta(this);
+      }
+
+      public LSystemString evaluate(Context context, Object[] inputs) {
+
+         LSystem lSystem = new LSystem ( ) ;
+         lSystem . addRule ( 'F' , "FF+F+F+F+F+F-F" ) ;
+         String out = lSystem . apply ( "F" , 5 ) ;
+         return new LSystemString ( out , Math . PI / 2 ) ;
+      }
+
+      public Class getReturnType() {
+         return LSystemString.class;
+      }
+
+   }
+
+   public static class Koch4 extends ExpressionFunction {
+
+      public Koch4() {
+         addGroupMeta(this);
+      }
+
+      public LSystemString evaluate(Context context, Object[] inputs) {
+
+         LSystem lSystem = new LSystem ( ) ;
+         lSystem . addRule ( 'F' , "FF+F++F+F" ) ;
+         String out = lSystem . apply ( "F+F+F+F" , 5 ) ;
+         return new LSystemString ( out , Math . PI / 2 ) ;
+      }
+
+      public Class getReturnType() {
+         return LSystemString.class;
+      }
+
+   }
+
+   public static class Koch5 extends ExpressionFunction {
+
+      public Koch5() {
+         addGroupMeta(this);
+      }
+
+      public LSystemString evaluate(Context context, Object[] inputs) {
+
+         LSystem lSystem = new LSystem ( ) ;
+         lSystem . addRule ( 'F' , "FF+F++F+F" ) ;
+         String out = lSystem . apply ( "F+F+F+F" , 5 ) ;
+         return new LSystemString ( out , 2 * Math . PI / 3 ) ;
+      }
+
+      public Class getReturnType() {
+         return LSystemString.class;
+      }
+
+   }
+
+   public static class Koch6 extends ExpressionFunction {
+
+      public Koch6() {
+         addGroupMeta(this);
+      }
+
+      public LSystemString evaluate(Context context, Object[] inputs) {
+
+         LSystem lSystem = new LSystem ( ) ;
+         lSystem . addRule ( 'F' , "F+FF++F+F" ) ;
+         String out = lSystem . apply ( "F" , 5 ) ;
+         return new LSystemString ( out , Math . PI / 2 ) ;
+      }
+
+      public Class getReturnType() {
+         return LSystemString.class;
+      }
+
+   }
+
+   public static class Serpinski1 extends ExpressionFunction {
+
+      public Serpinski1() {
+         addGroupMeta(this);
+      }
+
+      public LSystemString evaluate(Context context, Object[] inputs) {
+
+         LSystem lSystem = new LSystem ( ) ;
+         lSystem . addRule ( 'A' , "FB-FA-FB" ) ;
+         lSystem . addRule ( 'B' , "FA+FB+FA" ) ;
+         String out = lSystem . apply ( "A" , 6 ) ;
          return new LSystemString ( out , Math . PI / 6 ) ;
       }
 
@@ -101,16 +357,113 @@ public final class LSystems implements AllComponents<ExpressionFunction>, Descri
 
    }
 
+   public static class Plant2 extends ExpressionFunction {
+
+      public Plant2() {
+         addGroupMeta(this);
+      }
+
+      public LSystemString evaluate(Context context, Object[] inputs) {
+
+         LSystem lSystem = new LSystem ( ) ;
+         lSystem . addRule ( 'F' , "F[+F]F[-F]F" ) ;
+         String out = lSystem . apply ( "X" , 5 ) ;
+         return new LSystemString ( out , Math . PI / 7 ) ;
+      }
+
+      public Class getReturnType() {
+         return LSystemString.class;
+      }
+
+   }
+
+   public static class Plant3 extends ExpressionFunction {
+
+      public Plant3() {
+         addGroupMeta(this);
+      }
+
+      public LSystemString evaluate(Context context, Object[] inputs) {
+
+         LSystem lSystem = new LSystem ( ) ;
+         lSystem . addRule ( 'F' , "FF+[+F-F-F]-[F+F+F]" ) ;
+         String out = lSystem . apply ( "X" , 4 ) ;
+         return new LSystemString ( out , Math . PI / 8 ) ;
+      }
+
+      public Class getReturnType() {
+         return LSystemString.class;
+      }
+
+   }
+
+   public static class Plant4 extends ExpressionFunction {
+
+      public Plant4() {
+         addGroupMeta(this);
+      }
+
+      public LSystemString evaluate(Context context, Object[] inputs) {
+
+         LSystem lSystem = new LSystem ( ) ;
+         lSystem . addRule ( 'X' , "F[+X]F[-X]+X" ) ;
+         lSystem . addRule ( 'F' , "FF" ) ;
+         String out = lSystem . apply ( "X" , 7 ) ;
+         return new LSystemString ( out , Math . PI / 9 ) ;
+      }
+
+      public Class getReturnType() {
+         return LSystemString.class;
+      }
+
+   }
+
+   public static class Spiral extends ExpressionFunction {
+
+      public Spiral() {
+         addGroupMeta(this);
+      }
+
+      public LSystemString evaluate(Context context, Object[] inputs) {
+
+         LSystem lSystem = new LSystem ( ) ;
+         lSystem . addRule ( 'A' , "X+X+X+X+X+X+" ) ;
+         lSystem . addRule ( 'X' , "[F+F+F+F[---X-Y]+++++F++++++++F-F-F-F]" ) ;
+         lSystem . addRule ( 'Y' , "[F+F+F+F[---Y]+++++F++++++++F-F-F-F]" ) ;
+         String out = lSystem . apply ( "A" , 5 ) ;
+         return new LSystemString ( out , Math . PI / 12 ) ;
+      }
+
+      public Class getReturnType() {
+         return LSystemString.class;
+      }
+
+   }
+
    public String getDescription() {
       return "expressions for creating L-Systems";
    }
 
    public List<ExpressionFunction> allInstances(ContextModel cm) {
       List<ExpressionFunction> r = new ArrayList<ExpressionFunction>();
+      r.add(new PermuteAngles());
+      r.add(new LevyC());
+      r.add(new Pentigree());
+      r.add(new IslandLake());
+      r.add(new KochIsland1());
       r.add(new Koch1());
       r.add(new Koch2());
+      r.add(new Koch3());
+      r.add(new Koch4());
+      r.add(new Koch5());
+      r.add(new Koch6());
+      r.add(new Serpinski1());
       r.add(new Dragon());
       r.add(new Plant1());
+      r.add(new Plant2());
+      r.add(new Plant3());
+      r.add(new Plant4());
+      r.add(new Spiral());
       return r;
    }
 

44d1732e0b0d89658304f01b810230f209e604a5
ashmore <ashmore@a3f7fd50-973e-0410-8b28-ed0ccb69969c>
2010-05-07 05:31:06 +0000

    L-Systems do seem to be working.

--- PainterTools/src/painter/tools/brush/Brush.java
+++ PainterTools/src/painter/tools/brush/Brush.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package painter.tools.brush;
 
 import painter.tools.canvas.Canvas;
@@ -15,5 +14,4 @@ import utils.linear.Color;
 public interface Brush {
 
     void paint(double x, double y, double dx, double dy, double radius, Color color, Canvas canvas);
-
 }

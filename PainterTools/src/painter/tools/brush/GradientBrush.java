968642cb388f760a663b3f8eb4f94427450e3148
ashmore <ashmore@a3f7fd50-973e-0410-8b28-ed0ccb69969c>
2010-05-23 03:28:57 +0000

    Includes gradient brushes!

---
+++ PainterTools/src/painter/tools/brush/GradientBrush.java
@@ -0,0 +1,74 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package painter.tools.brush;
+
+import genetic.TerminationException;
+import java.awt.GradientPaint;
+import java.awt.Graphics2D;
+import painter.tools.canvas.Canvas;
+import utils.linear.Color;
+
+/**
+ * A very simple brush tool
+ * @author Calvin Ashmore
+ */
+abstract public class GradientBrush implements Brush {
+
+    private double radiusMultiplier = 1;
+    private BrushPositionPolicy positionPolicy;
+    private BrushSizePolicy sizePolicy;
+    private double angle;
+
+    public GradientBrush(BrushPositionPolicy positionPolicy, BrushSizePolicy sizePolicy) {
+        this.positionPolicy = positionPolicy;
+        this.sizePolicy = sizePolicy;
+    }
+
+    public GradientBrush(BrushPositionPolicy positionPolicy, BrushSizePolicy sizePolicy, double radiusMultiplier) {
+        this.positionPolicy = positionPolicy;
+        this.sizePolicy = sizePolicy;
+        this.radiusMultiplier = radiusMultiplier;
+    }
+
+    public void setAngle(double angle) {
+        this.angle = angle;
+    }
+
+    abstract public Color getColor2(Color baseColor);
+
+    public void paint(double x, double y, double dx, double dy, double radius, Color color1, Canvas canvas) {
+
+        if (Thread.interrupted()) {
+            throw new TerminationException();
+        }
+
+        Graphics2D graphics = canvas.getGraphics();
+
+        radius = Math.abs(radius * radiusMultiplier);
+
+        double width = radius * canvas.getHeight();
+        //double height = radius * canvas.getHeight();
+
+        x = positionPolicy.getX(x, y, canvas) + sizePolicy.getXOffset(width);
+        y = positionPolicy.getY(x, y, canvas) + sizePolicy.getYOffset(width);
+
+        float x1, x2, y1, y2;
+
+        x1 = (float) (x+.5*width*(1+Math.cos(angle)));
+        y1 = (float) (y+.5*width*(1+Math.sin(angle)));
+        x2 = (float) (x+.5*width*(1-Math.cos(angle)));
+        y2 = (float) (y+.5*width*(1-Math.sin(angle)));
+
+        Color color2 = getColor2(color1);
+        GradientPaint paint = new GradientPaint(x1, y1, new java.awt.Color(color1.toARGB()), x2, y2, new java.awt.Color(color2.toARGB()), true);
+
+        graphics.setPaint(paint);
+        graphics.fillOval(
+                (int) (x),
+                (int) (y),
+                (int) (width),
+                (int) (width));
+    }
+}

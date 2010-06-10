a0e332f338b18cd9b14785ce7c5d0ed403d5a422
ashmore <ashmore@a3f7fd50-973e-0410-8b28-ed0ccb69969c>
2010-06-10 22:55:45 +0000

    First dash at a more thorough program view interface.

---
+++ trunk/Painter/src/painter/app/ui/genview/ProgramPanel.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package painter.app.ui.genview;
+
+import genetic.GeneticTopLevel;
+import genetic.component.method.Method;
+import java.util.Map.Entry;
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Calvin Ashmore
+ */
+public class ProgramPanel extends JPanel {
+
+    private GeneticTopLevel program;
+
+    public ProgramPanel(GeneticTopLevel program) {
+        this.program = program;
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        // first context model for whole
+
+        add(new ContextModelPanel(program.getContextModel()));
+
+        for (Entry<String, Method> entry : program.getMethods().entrySet()) {
+            addMethod(entry.getKey(), entry.getValue());
+        }
+    }
+
+    private void addMethod(String methodName, Method method) {
+        MethodPanel mp = new MethodPanel(methodName, method);
+        add(mp);
+    }
+}
